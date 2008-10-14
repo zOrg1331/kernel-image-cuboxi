811f5922ef973f8502c828d6050272f605a7f567
Vitaliy Gusev <vgusev@openvz.org>
2008-10-14 19:20:33 +0400

    Adjust VE before call netdev_unregister_kobject/netdev_register_kobject
    These function use visible_net_class.
    
    http://bugzilla.openvz.org/show_bug.cgi?id=1044
    
    Signed-off-by: Vitaliy Gusev <vgusev@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@openvz.org>

--- net/core/dev.c
+++ net/core/dev.c
@@ -4528,8 +4528,11 @@ int __dev_change_net_namespace(struct net_device *dev, struct net *net, const ch
 	}
 
 	/* Fixup kobjects */
+	set_exec_env(src_ve);
 	netdev_unregister_kobject(dev);
+	set_exec_env(dst_ve);
 	err = netdev_register_kobject(dev);
+	set_exec_env(cur_ve);
 	WARN_ON(err);
 
 	/* Add the device back in the hashes */
