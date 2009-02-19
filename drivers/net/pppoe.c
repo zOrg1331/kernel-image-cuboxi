8207275f91f0b1849edc8cfdcfab4565997ce758
Cyrill Gorcunov <gorcunov@openvz.org>
2009-02-19 23:14:39 +0300

    net: PPPoE, PPPoL2TP - dont create socket without
    VE_FEATURE_PPP
    
    If environment has no VE_FEATURE_PPP feature set
    we should prohibit the creation of PPPoE, PPPoL2TP
    sockets.
    
    http://bugzilla.openvz.org/show_bug.cgi?id=1187
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@openvz.org>

--- drivers/net/pppoe.c
+++ drivers/net/pppoe.c
@@ -547,6 +547,9 @@ static int pppoe_create(struct net *net, struct socket *sock)
 {
 	struct sock *sk;
 
+	if (!(get_exec_env()->features & VE_FEATURE_PPP))
+		return -EACCES;
+
 	sk = sk_alloc(net, PF_PPPOX, GFP_KERNEL, &pppoe_sk_proto);
 	if (!sk)
 		return -ENOMEM;
