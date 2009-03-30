11e154b84769c247b57e751f485a610c8f8fa248
Philipp Reisner <philipp.reisner@linbit.com>
2009-03-30 18:47:22 +0200

    DRBD: misc
    buildtag.c tag will go away when we are not longer an external module.
    
    Signed-off-by: Philipp Reisner <philipp.reisner@linbit.com>
    Signed-off-by: Lars Ellenberg <lars.ellenberg@linbit.com>

---
+++ drivers/block/drbd/drbd_buildtag.c
@@ -0,0 +1,7 @@
+/* automatically generated. DO NOT EDIT. */
+#include <linux/drbd_config.h>
+const char *drbd_buildtag(void)
+{
+	return "GIT-hash: c74771beb9598144d31b861e7ea966f914914c4f drbd/drbd_actlog.c drbd/drbd_bitmap.c drbd/drbd_int.h drbd/drbd_main.c drbd/drbd_receiver.c drbd/drbd_req.c drbd/drbd_worker.c"
+		" build by phil@fat-tyre, 2009-03-30 16:54:38";
+}
