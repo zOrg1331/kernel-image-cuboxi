3c0c78db19bfeea32307efae10ef2a95f814ef2a
Jan Blunck <jblunck@suse.de>
2009-04-10 18:08:28 +0200

    squashfs: read directory entries from dcache
    When we create new files in a mounted squashfs, they
    only appear in the dcache, so we need to have a combined
    dcache_readdir plus squashfs_readdir_ondisk to get all of them.
    
    Signed-off-by: Jan Blunck <jblunck@suse.de>

--- fs/squashfs/dir.c
+++ fs/squashfs/dir.c
@@ -246,14 +246,110 @@ failed_read:
 	return 0;
 }
 
+/* Relationship between i_mode and the DT_xxx types */
+static inline unsigned char dt_type(struct inode *inode)
+{
+       return (inode->i_mode >> 12) & 15;
+}
+
+static int squashfs_readdir_cache(struct file *file, void *dirent,
+				  filldir_t filldir)
+{
+	struct dentry *dentry = file->f_path.dentry;
+	struct dentry *cursor = file->private_data;
+	struct list_head *p, *q = &cursor->d_u.d_child;
+
+	/* taken from dcache_readdir */
+	spin_lock(&dentry->d_lock);
+	/* FIXME: this is wrong, we should start after the on disk stuff */
+	if (file->f_pos == 2)
+		list_move(q, &dentry->d_subdirs);
+
+	for (p=q->next; p != &dentry->d_subdirs; p=p->next) {
+		struct dentry *next;
+		next = list_entry(p, struct dentry, d_u.d_child);
+		if (d_unhashed(next) || !next->d_inode || !next->d_fsdata)
+			continue;
+
+		spin_unlock(&dentry->d_lock);
+		if (filldir(dirent, next->d_name.name,
+			    next->d_name.len, file->f_pos,
+			    next->d_inode->i_ino,
+			    dt_type(next->d_inode)) < 0)
+			return 0;
+		spin_lock(&dentry->d_lock);
+		/* next is still alive */
+		list_move(q, p);
+		p = q;
+		file->f_pos++;
+	}
+	spin_unlock(&dentry->d_lock);
+	return 0;
+}
+
 static int squashfs_readdir(struct file *file, void *dirent, filldir_t filldir)
 {
-	return squashfs_readdir_ondisk(file->f_dentry, dirent,
-				       filldir, &file->f_pos);
+	loff_t offset = file->f_pos;
+	struct dentry *dentry = file->f_path.dentry;
+	int ret;
+
+	ret = 0;
+	if (offset < dentry->d_inode->i_size)
+		ret = squashfs_readdir_ondisk(dentry, dirent,
+					    filldir, &file->f_pos);
+	if (ret)
+		return ret;
+
+	return squashfs_readdir_cache(file, dirent, filldir);
+}
+
+static loff_t squashfs_dir_lseek(struct file *file, loff_t offset, int origin)
+{
+	struct inode *inode = file->f_path.dentry->d_inode;
+	mutex_lock(&inode->i_mutex);
+	switch (origin) {
+	case 1:
+		offset += file->f_pos;
+	case 0:
+		if (offset >= 0)
+			break;
+	default:
+		mutex_unlock(&inode->i_mutex);
+		return -EINVAL;
+	}
+
+	if (offset != file->f_pos) {
+		file->f_pos = offset;
+		if (file->f_pos >= inode->i_size) {
+			struct list_head *p;
+			struct dentry *cursor = file->private_data;
+			loff_t n = file->f_pos - inode->i_size;
+
+			spin_lock(&inode->i_lock);
+			list_del(&cursor->d_u.d_child);
+			p = file->f_path.dentry->d_subdirs.next;
+			while (n && p != &file->f_path.dentry->d_subdirs) {
+				struct dentry *next;
+				next = list_entry(p, struct dentry, d_u.d_child);
+
+				if (!d_unhashed(next) && next->d_inode &&
+				    next->d_fsdata)
+					n--;
+				p = p->next;
+			}
+			list_add_tail(&cursor->d_u.d_child, p);
+			spin_unlock(&inode->i_lock);
+		}
+	}
+	mutex_unlock(&inode->i_mutex);
+	return offset;
 }
 
 const struct file_operations squashfs_dir_ops = {
 	.read = generic_read_dir,
 	.readdir = squashfs_readdir,
-	.llseek = default_llseek,
+	.llseek = squashfs_dir_lseek,
+	.open = dcache_dir_open,
+	.release = dcache_dir_close,
+	.fsync = noop_fsync,
 };
