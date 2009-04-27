9f4259771faf1ffd99888796d925adae4c93630b
Kyle McMartin <kyle@phobos.i.jkkm.org>
2009-04-27 00:51:13 -0400

    parisc: add TIF_NOTIFY_RESUME and use tracehook_notify_resume
    Call tracehook_notify_resume() in do_notify_resume.
    
    Signed-off-by: Kyle McMartin <kyle@mcmartin.ca>

--- arch/parisc/include/asm/thread_info.h
+++ arch/parisc/include/asm/thread_info.h
@@ -59,6 +59,7 @@ struct thread_info {
 #define TIF_MEMDIE		5
 #define TIF_RESTORE_SIGMASK	6	/* restore saved signal mask */
 #define TIF_FREEZE		7	/* is freezing for suspend */
+#define TIF_NOTIFY_RESUME	8	/* tracing notification pending */
 
 #define _TIF_SYSCALL_TRACE	(1 << TIF_SYSCALL_TRACE)
 #define _TIF_SIGPENDING		(1 << TIF_SIGPENDING)
@@ -67,9 +68,10 @@ struct thread_info {
 #define _TIF_32BIT		(1 << TIF_32BIT)
 #define _TIF_RESTORE_SIGMASK	(1 << TIF_RESTORE_SIGMASK)
 #define _TIF_FREEZE		(1 << TIF_FREEZE)
+#define _TIF_NOTIFY_RESUME	(1 << TIF_NOTIFY_RESUME)
 
-#define _TIF_USER_WORK_MASK     (_TIF_SIGPENDING | \
-                                 _TIF_NEED_RESCHED | _TIF_RESTORE_SIGMASK)
+#define _TIF_DO_NOTIFY_RESUME_MASK	(_TIF_SIGPENDING | _TIF_RESTORE_SIGMASK | \
+						_TIF_NOTIFY_RESUME)
 
 #endif /* __KERNEL__ */
 
