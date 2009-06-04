9d9949ce45b42be7a8157ea00fff2b1005ed844a
Steven Rostedt <srostedt@redhat.com>
2009-06-04 00:55:45 -0400

    tracing/function-profiler: do not free per cpu variable stat
    The per cpu variable stat is freeded if we fail to allocate a name
    on start up. This was due to stat at first being allocated in the
    initial design. But since then, it has become a static per cpu variable
    but the free on error was not removed.
    
    Also added __init annotation to the function that this is in.
    
    [ Impact: prevent possible memory corruption on low mem at boot up ]
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- kernel/trace/ftrace.c
+++ kernel/trace/ftrace.c
@@ -766,7 +766,7 @@ static struct tracer_stat function_stats __initdata = {
 	.stat_show	= function_stat_show
 };
 
-static void ftrace_profile_debugfs(struct dentry *d_tracer)
+static __init void ftrace_profile_debugfs(struct dentry *d_tracer)
 {
 	struct ftrace_profile_stat *stat;
 	struct dentry *entry;
@@ -784,7 +784,6 @@ static void ftrace_profile_debugfs(struct dentry *d_tracer)
 			 * The files created are permanent, if something happens
 			 * we still do not free memory.
 			 */
-			kfree(stat);
 			WARN(1,
 			     "Could not allocate stat file for cpu %d\n",
 			     cpu);
@@ -811,7 +810,7 @@ static void ftrace_profile_debugfs(struct dentry *d_tracer)
 }
 
 #else /* CONFIG_FUNCTION_PROFILER */
-static void ftrace_profile_debugfs(struct dentry *d_tracer)
+static __init void ftrace_profile_debugfs(struct dentry *d_tracer)
 {
 }
 #endif /* CONFIG_FUNCTION_PROFILER */
