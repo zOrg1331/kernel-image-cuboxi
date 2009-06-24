3edd8b5501c0bd46fcf190063b6d5db4d499a507
David Brownell <dbrownell@users.sourceforge.net>
2009-06-24 10:06:31 -0700

    driver model: constify attribute groups
    Let attribute group vectors be declared "const".  We'd
    like to let most attribute metadata live in read-only
    sections... this is a start.
    
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/s390/cio/device.c
+++ drivers/s390/cio/device.c
@@ -624,7 +624,7 @@ static struct attribute_group ccwdev_attr_group = {
 	.attrs = ccwdev_attrs,
 };
 
-static struct attribute_group *ccwdev_attr_groups[] = {
+static const struct attribute_group *ccwdev_attr_groups[] = {
 	&ccwdev_attr_group,
 	NULL,
 };
