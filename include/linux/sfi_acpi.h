f9d2185c9a1af711d4fde9f4948b247402d225ed
Feng Tang <feng.tang@intel.com>
2009-07-13 16:00:36 +0800

    SFI, PCI: Introduce a x86_table_parser()
    This new parser is used to parse the ACPI tables that are also
    used by SFI. It will first try the ACPI parsing method, then try
    SFI method if prvious one fails
    
    Signed-off-by: Feng Tang <feng.tang@intel.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- include/linux/sfi_acpi.h
+++ include/linux/sfi_acpi.h
@@ -47,6 +47,14 @@ extern int sfi_acpi_table_parse(char *signature, char *oem_id,
 				char *oem_table_id,
 				int (*handler)(struct acpi_table_header *));
 
+static inline int x86_table_parse(char *signature,
+				int (*handler)(struct acpi_table_header *))
+{
+	if (!acpi_table_parse(signature, handler))
+		return 0;
+
+	return sfi_acpi_table_parse(signature, NULL, NULL, handler);
+}
 #else /* !CONFIG_SFI */
 
 static inline int sfi_acpi_table_parse(char *signature, char *oem_id,
@@ -56,6 +64,11 @@ static inline int sfi_acpi_table_parse(char *signature, char *oem_id,
 	return -1;
 }
 
+static inline int x86_table_parse(char *signature,
+				int (*handler)(struct acpi_table_header *))
+{
+	return acpi_table_parse(signature, handler);
+}
 #endif /* CONFIG_SFI */
 
 #endif /*_LINUX_SFI_ACPI_H*/
