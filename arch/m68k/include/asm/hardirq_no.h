5bed70fc0d25acfbca1604063e61658900a79abb
Christoph Hellwig <hch@lst.de>
2009-08-04 16:52:44 +0200

    m68knommu: convert to asm-generic/hardirq.h
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Greg Ungerer <gerg@uclinux.org>

--- arch/m68k/include/asm/hardirq_no.h
+++ arch/m68k/include/asm/hardirq_no.h
@@ -1,15 +1,7 @@
 #ifndef __M68K_HARDIRQ_H
 #define __M68K_HARDIRQ_H
 
-#include <linux/cache.h>
-#include <linux/threads.h>
 #include <asm/irq.h>
-
-typedef struct {
-	unsigned int __softirq_pending;
-} ____cacheline_aligned irq_cpustat_t;
-
-#include <linux/irq_cpustat.h>	/* Standard mappings for irq_cpustat_t above */
 
 #define HARDIRQ_BITS	8
 
@@ -23,5 +15,8 @@ typedef struct {
 #endif
 
 void ack_bad_irq(unsigned int irq);
+#define ack_bad_irq ack_bad_irq
+
+#include <asm-generic/hardirq.h>
 
 #endif /* __M68K_HARDIRQ_H */
