vaesumed/linux-xlnx

fsnotify: allow addition of duplicate fsnotify marks
This patch allows a task to add a second fsnotify mark to an inode for the
same group.  This mark will be added to the end of the inode's list and
this will never be found by the stand fsnotify_find_mark() function.   This
is useful if a user wants to add a new mark before removing the old one.

Signed-off-by: Eric Paris <eparis@redhat.com>

fs/notify/inotify/inotify_user.c
@@ -445,7 +445,7 @@ find_entry:
 			goto out_err;
 		}
 
-		ret = fsnotify_add_mark(&ientry->fsn_entry, group, inode);
+		ret = fsnotify_add_mark(&ientry->fsn_entry, group, inode, 0);
 		if (ret == -EEXIST)
 			goto find_entry;
 		else if (ret)

zOrg1331/kernel-image-cuboxi

fs/notify/inotify/inotify_user.c
@@ -568,7 +568,7 @@ retry:
 	fsnotify_get_mark(&tmp_ientry->fsn_entry);
 
 	/* we are on the idr, now get on the inode */
-	ret = fsnotify_add_mark(&tmp_ientry->fsn_entry, group, inode);
+	ret = fsnotify_add_mark(&tmp_ientry->fsn_entry, group, inode, 0);
 	if (ret) {
 		/* we failed to get on the inode, get off the idr */
 		inotify_remove_from_idr(group, tmp_ientry);