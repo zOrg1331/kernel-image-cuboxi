c75429b46af611833c4c0dcb7929cfccd03b4e92
Stephen Rothwell <sfr@canb.auug.org.au>
2009-11-11 20:08:46 +1100

    Revert "x86: Handle HW IOMMU initialization failure gracefully"
    This reverts commit 75f1cdf1dda92cae037ec848ae63690d91913eac.

--- arch/x86/kernel/pci-nommu.c
+++ arch/x86/kernel/pci-nommu.c
@@ -103,3 +103,12 @@ struct dma_map_ops nommu_dma_ops = {
 	.sync_sg_for_device	= nommu_sync_sg_for_device,
 	.is_phys		= 1,
 };
+
+void __init no_iommu_init(void)
+{
+	if (dma_ops)
+		return;
+
+	force_iommu = 0; /* no HW IOMMU */
+	dma_ops = &nommu_dma_ops;
+}
