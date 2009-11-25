cc187de51404cb76e1ed6dfb030b95ba3f6eac67
Stephen Rothwell <sfr@canb.auug.org.au>
2009-11-25 23:05:11 +1100

    Revert "Merge branch 'for-linus' into for-next to receive per_cpu_ptr_to_phys()."
    This reverts commit 25b1a9ccfdd0e3dc306a1e3a354ba5480c4b0dd9, reversing
    changes made to 809fff2143c523cb313ba320ab6aaa3ac2e9e958.

--- include/linux/percpu.h
+++ include/linux/percpu.h
@@ -137,7 +137,6 @@ extern int __init pcpu_page_first_chunk(size_t reserved_size,
 extern void __percpu *__alloc_reserved_percpu(size_t size, size_t align);
 extern void __percpu *__alloc_percpu(size_t size, size_t align);
 extern void free_percpu(void __percpu *__pdata);
-extern phys_addr_t per_cpu_ptr_to_phys(void *addr);
 
 #ifndef CONFIG_HAVE_SETUP_PER_CPU_AREA
 extern void __init setup_per_cpu_areas(void);
