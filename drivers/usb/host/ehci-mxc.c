505fa3fd052865ef22552fecca71d37fa0c551a8
Valentin Longchamp <valentin.longchamp@epfl.ch>
2009-12-02 17:13:01 +0100

    USB: fix occasional ULPI timeouts with ehci-mxc
    On various mxc boards, the intial ULPI reads resulted in a timeout
    which prevented the transceiver to be identified and thus the ehci
    device to be probed.
    
    Initializing the hardware lines connected to the transceiver (through
    pdata->init call) before actually enabling clocks and configuring
    registers in the devices fixes this problem.
    
    Signed-off-by: Valentin Longchamp <valentin.longchamp@epfl.ch>
    Acked-by: Daniel Mack <daniel@caiaq.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/host/ehci-mxc.c
+++ drivers/usb/host/ehci-mxc.c
@@ -162,6 +162,15 @@ static int ehci_mxc_drv_probe(struct platform_device *pdev)
 		goto err_ioremap;
 	}
 
+	/* call platform specific init function */
+	if (pdata->init) {
+		ret = pdata->init(pdev);
+		if (ret) {
+			dev_err(dev, "platform init failed\n");
+			goto err_init;
+		}
+	}
+
 	/* enable clocks */
 	priv->usbclk = clk_get(dev, "usb");
 	if (IS_ERR(priv->usbclk)) {
@@ -191,15 +200,6 @@ static int ehci_mxc_drv_probe(struct platform_device *pdev)
 	ret = mxc_set_usbcontrol(pdev->id, pdata->flags);
 	if (ret < 0)
 		goto err_init;
-
-	/* call platform specific init function */
-	if (pdata->init) {
-		ret = pdata->init(pdev);
-		if (ret) {
-			dev_err(dev, "platform init failed\n");
-			goto err_init;
-		}
-	}
 
 	/* most platforms need some time to settle changed IO settings */
 	mdelay(10);
