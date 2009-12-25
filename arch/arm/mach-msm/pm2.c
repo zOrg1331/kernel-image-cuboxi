5a07cae49062f4761ea09f33888aa776951478af
Daniel Walker <dwalker@codeaurora.org>
2009-12-25 14:42:06 -0800

    arm: msm: add pm_restart argument
    Signed-off-by: Daniel Walker <c_dwalke@quicinc.com>

--- arch/arm/mach-msm/pm2.c
+++ arch/arm/mach-msm/pm2.c
@@ -1554,7 +1554,7 @@ static void msm_pm_power_off(void)
 		;
 }
 
-static void msm_pm_restart(char str)
+static void msm_pm_restart(char str, const char *cmd )
 {
 	msm_proc_comm(PCOM_RESET_CHIP, &restart_reason, 0);
 
