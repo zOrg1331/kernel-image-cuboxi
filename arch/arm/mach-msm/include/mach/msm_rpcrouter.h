bfb6e8a4076056d4dc76aa799f9ec0bcd765089d
Daniel Walker <c_dwalke@quicinc.com>
2009-12-28 16:01:47 -0800

    Add missing rpc macros.

--- arch/arm/mach-msm/include/mach/msm_rpcrouter.h
+++ arch/arm/mach-msm/include/mach/msm_rpcrouter.h
@@ -28,6 +28,25 @@ struct rpcrouter_ioctl_server_args {
 	uint32_t vers;
 };
 
+#define RPC_ROUTER_VERSION_V1 0x00010000
+
+#define RPC_ROUTER_IOCTL_MAGIC (0xC1)
+
+#define RPC_ROUTER_IOCTL_GET_VERSION \
+	_IOR(RPC_ROUTER_IOCTL_MAGIC, 0, unsigned int)
+
+#define RPC_ROUTER_IOCTL_GET_MTU \
+	_IOR(RPC_ROUTER_IOCTL_MAGIC, 1, unsigned int)
+
+#define RPC_ROUTER_IOCTL_REGISTER_SERVER \
+	_IOWR(RPC_ROUTER_IOCTL_MAGIC, 2, unsigned int)
+
+#define RPC_ROUTER_IOCTL_UNREGISTER_SERVER \
+	_IOWR(RPC_ROUTER_IOCTL_MAGIC, 3, unsigned int)
+
+#define RPC_ROUTER_IOCTL_CLEAR_NETRESET \
+	_IOWR(RPC_ROUTER_IOCTL_MAGIC, 4, unsigned int)
+
 /* RPC API version structure
  * Version bit 31 : 1->hashkey versioning,
  *                  0->major-minor (backward compatible) versioning
