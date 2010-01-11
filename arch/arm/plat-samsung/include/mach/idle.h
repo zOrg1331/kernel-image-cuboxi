7b5f732f17a63b498a86a0d84f0e4c8d607fc8b3
Kukjin Kim <kgene.kim@samsung.com>
2010-01-11 12:21:53 +0900

    ARM: SAMSUNG: Add idle support
    This patch adds common idle functionality for all Samsung SoC's.
    
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

---
+++ arch/arm/plat-samsung/include/mach/idle.h
@@ -0,0 +1,18 @@
+/* linux/arch/arm/plat-samsung/include/mach/idle.h
+ *
+ * Copyright (c) 2009 Samsung Electronics Co., Ltd.
+ *		http://www.samsung.com/
+ *
+ * Idle support
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+*/
+
+#ifndef __ASM_PLAT_IDLE_H
+#define __ASM_PLAT_IDLE_H __FILE__
+
+extern void (*s3c_idle_fn)(void);
+
+#endif /* __ASM_PLAT_IDLE_H */
