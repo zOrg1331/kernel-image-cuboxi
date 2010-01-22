5f4a255844ec2709edc4c1f364689f4d8e40460f
Theodore Ts'o <tytso@mit.edu>
2010-01-22 18:55:34 -0500

    ext4: Fix ext4_end_io_work so we don't iput the inode while we hold the mutex
    This should be merged into a previous patch eventually, but I'm
    keeping it separate for now.
    
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -3590,24 +3590,25 @@ static int ext4_end_io_nolock(ext4_io_end_t *io)
  */
 static void ext4_end_io_work(struct work_struct *work)
 {
-	ext4_io_end_t *io  = container_of(work, ext4_io_end_t, work);
-	struct inode *inode = io->inode;
-	int ret = 0;
+	ext4_io_end_t		*io = container_of(work, ext4_io_end_t, work);
+	struct inode		*inode = io->inode;
+	struct ext4_inode_info	*ei = EXT4_I(inode);
+	unsigned long		flags;
+	int			ret;
 
 	mutex_lock(&inode->i_mutex);
 	ret = ext4_end_io_nolock(io);
-	if (ret >= 0) {
-		unsigned long flags;
-		struct ext4_inode_info *ei = EXT4_I(inode);
-
-		spin_lock_irqsave(&ei->i_completed_io_lock, flags);
-		if (!list_empty(&io->list))
-			list_del_init(&io->list);
-		spin_unlock_irqrestore(&ei->i_completed_io_lock, flags);
-
-		ext4_free_io_end(io);
+	if (ret < 0) {
+		mutex_unlock(&inode->i_mutex);
+		return;
 	}
+
+	spin_lock_irqsave(&ei->i_completed_io_lock, flags);
+	if (!list_empty(&io->list))
+		list_del_init(&io->list);
+	spin_unlock_irqrestore(&ei->i_completed_io_lock, flags);
 	mutex_unlock(&inode->i_mutex);
+	ext4_free_io_end(io);
 }
 
 /*
