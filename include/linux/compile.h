b7354d29bd84899fa443c57ae11235df4602c28b
Stephen Rothwell <sfr@canb.auug.org.au>
2010-01-22 11:31:05 +1100

    Revert "PCI/ISDN: push deprecated pci_find_device() function to last user (isdn)"
    This reverts commit e303d9eadc64907535a0a104afa5871d2b83275e.

--- include/linux/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 6 */
-/* SMP */
-#define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#6 SMP Wed Dec 16 13:46:56 PST 2009"
-#define LINUX_COMPILE_TIME "13:46:56"
-#define LINUX_COMPILE_BY "jbarnes"
-#define LINUX_COMPILE_HOST "jbarnes-piketon"
-#define LINUX_COMPILE_DOMAIN
-#define LINUX_COMPILER "gcc version 4.4.1 (Ubuntu 4.4.1-4ubuntu8) "
