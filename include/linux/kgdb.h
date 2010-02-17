d87773109eb67209b08a5ecd2476b839d6d35afe
Jason Wessel <jason.wessel@windriver.com>
2010-02-17 16:05:34 -0600

    kgdboc,debug_core: Add call backs to allow kernel mode switching
    Hooks for the kgdb core for registration of the drm layer to provide a
    call backs so as to perform kernel mode switching, for use with kdb.
    
    [jbarnes@virtuousgeek.org: add ops arg to kgdb console active & restore hooks]
    
    CC: Jesse Barnes <jbarnes@virtuousgeek.org>
    Signed-off-by: Jason Wessel <jason.wessel@windriver.com>

--- include/linux/kgdb.h
+++ include/linux/kgdb.h
@@ -283,9 +283,31 @@ extern int kgdb_nmicallback(int cpu, void *regs);
 
 extern int			kgdb_single_step;
 extern atomic_t			kgdb_active;
+#endif /* CONFIG_KGDB */
+
+/* Common to all that include kgdb.h */
+struct dbg_kms_ops {
+	int (*activate_console) (struct dbg_kms_ops *ops);
+	int (*restore_console) (struct dbg_kms_ops *ops);
+};
+
+#ifdef CONFIG_KGDB
 #define in_dbg_master() \
 	(raw_smp_processor_id() == atomic_read(&kgdb_active))
+
+extern struct dbg_kms_ops *dbg_kms_ops;
+extern int dbg_kms_ops_register(struct dbg_kms_ops *ops);
+extern int dbg_kms_ops_unregister(struct dbg_kms_ops *ops);
 #else /* ! CONFIG_KGDB */
 #define in_dbg_master() (0)
+
+static inline int dbg_kms_ops_register(struct dbg_kms_ops *ops)
+{
+	return 0;
+}
+static inline int dbg_kms_ops_unregister(struct dbg_kms_ops *ops)
+{
+	return 0;
+}
 #endif /* ! CONFIG_KGDB */
 #endif /* _KGDB_H_ */
