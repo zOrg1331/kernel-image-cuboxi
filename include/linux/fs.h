7805f36534f20e530fb84e83a360993ec78f3bb6
Denis Lunev <den@openvz.org>
2010-02-27 16:57:56 +0300

    ve: vfs sillyrename
    i_nlink count on private inodes after silly rename is 1. So, virtual inodes
    gain i_nlink == 1 and remains in unused_list instead of to be cleaned.
    
    Bug #114672 #112999
    
    Signed-off-by: Denis V. Lunev <den@parallels.com>
    Signed-off-by: Pavel Emelyanov <xemul@openvz.org>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -240,6 +240,9 @@ struct inodes_stat_t {
 #define S_SWAPFILE	256	/* Do not truncate: swapon got its bmaps */
 #define S_PRIVATE	512	/* Inode is fs-internal */
 
+/* VZ flags -- These are not upstream! */
+#define S_NOUNUSE	(1 << 17) /* just destroy inode in cleanup */
+
 /*
  * Note that nosuid etc flags are inode-specific: setting some file-system
  * flags just means all the inodes inherit those flags by default. It might be
