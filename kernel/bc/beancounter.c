1ff4faada1dabfdc4592e2824ce53a357373c83e
Konstantin Khlebnikov <khlebnikov@openvz.org>
2010-02-27 16:57:49 +0300

    bc: pb hash cookie
    add random hash cookie to ub to use in pb_hash instead of non-random ub_uid
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@openvz.org>

--- kernel/bc/beancounter.c
+++ kernel/bc/beancounter.c
@@ -31,6 +31,7 @@
 #include <linux/module.h>
 #include <linux/mm.h>
 #include <linux/sched.h>
+#include <linux/random.h>
 
 #include <bc/beancounter.h>
 #include <bc/hash.h>
@@ -600,6 +601,7 @@ EXPORT_SYMBOL(ub_ratelimit);
 static void init_beancounter_struct(struct user_beancounter *ub)
 {
 	ub->ub_magic = UB_MAGIC;
+	ub->ub_cookie = get_random_int();
 	atomic_set(&ub->ub_refcount, 1);
 	spin_lock_init(&ub->ub_lock);
 	INIT_LIST_HEAD(&ub->ub_tcp_sk_list);
