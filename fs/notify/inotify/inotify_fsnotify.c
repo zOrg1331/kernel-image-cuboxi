01dbf0ef9b8bb3c85c41a1227d89d9bc6a750446
Pavel Emelyanov <xemul@openvz.org>
2010-03-11 15:31:50 +0300

    inotify: Removed lost debugging that broke compilation
    Signed-off-by: Pavel Emelyanov <xemul@openvz.org>

--- fs/notify/inotify/inotify_fsnotify.c
+++ fs/notify/inotify/inotify_fsnotify.c
@@ -147,7 +147,7 @@ static void inotify_free_group_priv(struct fsnotify_group *group)
 	idr_for_each(&group->inotify_data.idr, idr_callback, group);
 	idr_remove_all(&group->inotify_data.idr);
 	idr_destroy(&group->inotify_data.idr);
-	free_uid(group->inotify_data.user, UID_INOTIFY);
+	free_uid(group->inotify_data.user);
 }
 
 void inotify_free_event_priv(struct fsnotify_event_private_data *fsn_event_priv)
