4bfffe798529d36ddb57c40106a70ca041c514e4
Wolfram Sang <w.sang@pengutronix.de>
2010-05-21 06:52:06 +0200

    pcm037/pcm043/pca100: fix ULPI-related build warnings
    Like this one (if CONFIG_USB_ULPI is not set):
    
    	arch/arm/mach-mx3/mach-pcm043.c:312: warning: 'otg_pdata' defined but not used
    	arch/arm/mach-mx3/mach-pcm043.c:317: warning: 'usbh1_pdata' defined but not used
    
    Signed-off-by: Wolfram Sang <w.sang@pengutronix.de>
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>

--- arch/arm/mach-mx3/mach-pcm043.c
+++ arch/arm/mach-mx3/mach-pcm043.c
@@ -308,6 +308,7 @@ static struct mxc_nand_platform_data pcm037_nand_board_info = {
 	.hw_ecc = 1,
 };
 
+#if defined(CONFIG_USB_ULPI)
 static struct mxc_usbh_platform_data otg_pdata = {
 	.portsc	= MXC_EHCI_MODE_UTMI,
 	.flags	= MXC_EHCI_INTERFACE_DIFF_UNI,
@@ -318,6 +319,7 @@ static struct mxc_usbh_platform_data usbh1_pdata = {
 	.flags	= MXC_EHCI_INTERFACE_SINGLE_UNI | MXC_EHCI_INTERNAL_PHY |
 		  MXC_EHCI_IPPUE_DOWN,
 };
+#endif
 
 static struct fsl_usb2_platform_data otg_device_pdata = {
 	.operating_mode = FSL_USB2_DR_DEVICE,
