a73eeb8010b8913f828123dd77a71664e261356d
Stephen Rothwell <sfr@canb.auug.org.au>
2010-05-21 11:14:45 +1000

    Revert "ecryptfs: disallow ecryptfs as underlying filesystem"
    This reverts commit ae0660764caffebba056214a8450e7217430939f.

--- fs/ecryptfs/main.c
+++ fs/ecryptfs/main.c
@@ -602,46 +602,28 @@ static int ecryptfs_get_sb(struct file_system_type *fs_type, int flags,
 			struct vfsmount *mnt)
 {
 	int rc;
-	struct super_block *sb, *lower_sb;
-	struct nameidata nd;
-
-	rc = path_lookup(dev_name, LOOKUP_FOLLOW | LOOKUP_DIRECTORY, &nd);
-	if (rc) {
-		printk(KERN_WARNING
-			"path_lookup() failed on dev_name = [%s]\n", dev_name);
-		goto out;
-	}
-	lower_sb = nd.path.dentry->d_sb;
-	if (strcmp(lower_sb->s_type->name, "ecryptfs") == 0) {
-		rc = -EINVAL;
-		printk(KERN_ERR "Mount on filesystem of type "
-			"eCryptfs explicitly disallowed due to "
-			"known incompatibilities\n");
-		goto out_pathput;
-	}
+	struct super_block *sb;
 
 	rc = get_sb_nodev(fs_type, flags, raw_data, ecryptfs_fill_super, mnt);
 	if (rc < 0) {
 		printk(KERN_ERR "Getting sb failed; rc = [%d]\n", rc);
-		goto out_pathput;
+		goto out;
 	}
 	sb = mnt->mnt_sb;
 	rc = ecryptfs_parse_options(sb, raw_data);
 	if (rc) {
 		printk(KERN_ERR "Error parsing options; rc = [%d]\n", rc);
-		goto out_dput;
+		goto out_abort;
 	}
 	rc = ecryptfs_read_super(sb, dev_name);
 	if (rc) {
 		printk(KERN_ERR "Reading sb failed; rc = [%d]\n", rc);
-		goto out_dput;
+		goto out_abort;
 	}
 	goto out;
-out_dput:
+out_abort:
 	dput(sb->s_root); /* aka mnt->mnt_root, as set by get_sb_nodev() */
 	deactivate_locked_super(sb);
-out_pathput:
-	path_put(&nd.path);
 out:
 	return rc;
 }
