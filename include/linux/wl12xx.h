7e2461630d7cb6136a77a88bef95b66533820907
Stephen Rothwell <sfr@canb.auug.org.au>
2010-09-27 15:39:08 +1000

    Revert "wl12xx: add platform data passing support"
    This reverts commit 61ee7007a5d61aa066076da578e8e8084e122d7d.

--- include/linux/wl12xx.h
+++ include/linux/wl12xx.h
@@ -32,7 +32,4 @@ struct wl12xx_platform_data {
 	int board_ref_clock;
 };
 
-int wl12xx_set_platform_data(const struct wl12xx_platform_data *data);
-const struct wl12xx_platform_data *wl12xx_get_platform_data(void);
-
 #endif
