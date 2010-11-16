cfe2af20eb1b6e4213a5d060731cd3060f5f4b78
Linus Walleij <linus.walleij@stericsson.com>
2010-11-16 17:56:42 +0100

    mmc: clean up clock-gating code
    This rids the unused boolean clk_pending_gate from the code and renames
    the clk_disable_work to clk_gate_work in analogy with the other names
    used for gating code.
    
    Signed-off-by: Linus Walleij <linus.walleij@stericsson.com>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -175,8 +175,7 @@ struct mmc_host {
 	int			clk_requests;	/* internal reference counter */
 	unsigned int		clk_delay;	/* number of MCI clk hold cycles */
 	bool			clk_gated;	/* clock gated */
-	bool			clk_pending_gate; /* pending clock gating */
-	struct work_struct	clk_disable_work; /* delayed clock disable */
+	struct work_struct	clk_gate_work; /* delayed clock gate */
 	unsigned int		clk_old;	/* old clock value cache */
 	spinlock_t		clk_lock;	/* lock for clk fields */
 #endif
