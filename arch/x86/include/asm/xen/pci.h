af8570301312758d3e027ac08cd4edcad59d3dd3
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2010-12-14 13:59:16 -0500

    xen/pci: Fix compile errors.
    In the past we used to have much more of the CONFIG_XEN_*. We have
    trimmed them down (yeey), but forgot to move around one of the header
    files. This also fixes:
    
    linux-next-20101214/arch/x86/pci/xen.c:451: error: redefinition of 'xen_find_device_domain_owner'
    linux-next-20101214/arch/x86/include/asm/xen/pci.h:25: note: previous definition of 'xen_find_device_domain_owner' was here
    linux-next-20101214/arch/x86/pci/xen.c:465: error: redefinition of 'xen_register_device_domain_owner'
    linux-next-20101214/arch/x86/include/asm/xen/pci.h:29: note: previous definition of 'xen_register_device_domain_owner' was here
    linux-next-20101214/arch/x86/pci/xen.c:487: error: redefinition of 'xen_unregister_device_domain_owner'
    linux-next-20101214/arch/x86/include/asm/xen/pci.h:34: note: previous definition of 'xen_unregister_device_domain_owner' was here
    
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Reported-by: Randy Dunlap <randy.dunlap@oracle.com>

--- arch/x86/include/asm/xen/pci.h
+++ arch/x86/include/asm/xen/pci.h
@@ -5,22 +5,15 @@
 extern int __init pci_xen_init(void);
 extern int __init pci_xen_hvm_init(void);
 #define pci_xen 1
+int xen_find_device_domain_owner(struct pci_dev *dev);
+int xen_register_device_domain_owner(struct pci_dev *dev, uint16_t domain);
+int xen_unregister_device_domain_owner(struct pci_dev *dev);
 #else
 #define pci_xen 0
 #define pci_xen_init (0)
 static inline int pci_xen_hvm_init(void)
 {
 	return -1;
-}
-#endif
-#if defined(CONFIG_XEN_DOM0)
-void __init xen_setup_pirqs(void);
-int xen_find_device_domain_owner(struct pci_dev *dev);
-int xen_register_device_domain_owner(struct pci_dev *dev, uint16_t domain);
-int xen_unregister_device_domain_owner(struct pci_dev *dev);
-#else
-static inline void __init xen_setup_pirqs(void)
-{
 }
 static inline int xen_find_device_domain_owner(struct pci_dev *dev)
 {
@@ -34,6 +27,14 @@ static inline int xen_register_device_domain_owner(struct pci_dev *dev,
 static inline int xen_unregister_device_domain_owner(struct pci_dev *dev)
 {
 	return -1;
+}
+#endif
+
+#if defined(CONFIG_XEN_DOM0)
+void __init xen_setup_pirqs(void);
+#else
+static inline void __init xen_setup_pirqs(void)
+{
 }
 #endif
 
