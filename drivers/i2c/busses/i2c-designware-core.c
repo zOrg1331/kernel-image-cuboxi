d16f4194c73afd9134cb42e41a80fb3fd2a5e937
Dirk Brandewie <dirk.brandewie@gmail.com>
2011-03-12 12:23:26 -0800

    i2c-designware: Add runtime power management support
    Add runtime power management to the PCI driver.
    
    Signed-off-by: Dirk Brandewie <dirk.brandewie@gmail.com>
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

--- drivers/i2c/busses/i2c-designware-core.c
+++ drivers/i2c/busses/i2c-designware-core.c
@@ -31,6 +31,7 @@
 #include <linux/i2c.h>
 #include <linux/interrupt.h>
 #include <linux/io.h>
+#include <linux/pm_runtime.h>
 #include "i2c-designware-core.h"
 
 /*
@@ -500,6 +501,7 @@ i2c_dw_xfer(struct i2c_adapter *adap, struct i2c_msg msgs[], int num)
 	dev_dbg(dev->dev, "%s: msgs: %d\n", __func__, num);
 
 	mutex_lock(&dev->lock);
+	pm_runtime_get_sync(dev->dev);
 
 	INIT_COMPLETION(dev->cmd_complete);
 	dev->msgs = msgs;
@@ -549,6 +551,7 @@ i2c_dw_xfer(struct i2c_adapter *adap, struct i2c_msg msgs[], int num)
 	ret = -EIO;
 
 done:
+	pm_runtime_put(dev->dev);
 	mutex_unlock(&dev->lock);
 
 	return ret;
@@ -670,10 +673,13 @@ void i2c_dw_enable(struct dw_i2c_dev *dev)
 	dw_writel(dev, 1, DW_IC_ENABLE);
 }
 
+u32 i2c_dw_is_enabled(struct dw_i2c_dev *dev)
+{
+	return dw_readl(dev, DW_IC_ENABLE);
+}
+
 void i2c_dw_disable(struct dw_i2c_dev *dev)
 {
-	int ret;
-
 	/* Disable controller */
 	dw_writel(dev, 0, DW_IC_ENABLE);
 
