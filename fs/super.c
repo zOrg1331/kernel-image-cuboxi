cc065951c09aa42186a81e3e78a430f0ab7563b0
Jeff Layton <jlayton@redhat.com>
2011-03-29 09:33:31 -0400

    VFS: trivial: fix comment on s_maxbytes value warning check
    I originally intended to remove this warning in 2.6.34, but it's not in
    a high performance codepath and might help us to catch bugs later. Let's
    keep it, but fix the comment to allay confusion about its removal.
    
    Signed-off-by: Jeff Layton <jlayton@redhat.com>
    Signed-off-by: Steve French <sfrench@us.ibm.com>

--- fs/super.c
+++ fs/super.c
@@ -1013,8 +1013,7 @@ vfs_kern_mount(struct file_system_type *type, int flags, const char *name, void 
 	 * filesystems should never set s_maxbytes larger than MAX_LFS_FILESIZE
 	 * but s_maxbytes was an unsigned long long for many releases. Throw
 	 * this warning for a little while to try and catch filesystems that
-	 * violate this rule. This warning should be either removed or
-	 * converted to a BUG() in 2.6.34.
+	 * violate this rule.
 	 */
 	WARN((mnt->mnt_sb->s_maxbytes < 0), "%s set sb->s_maxbytes to "
 		"negative value (%lld)\n", type->name, mnt->mnt_sb->s_maxbytes);
