c2508f424f75cb6a6cf74580a89a9cbe37363e72
Anton Protopopov <aspsk@altlinux.org>
2012-01-23 13:43:41 +0400

    Fix build

--- fs/ocfs2/super.c
+++ fs/ocfs2/super.c
@@ -2229,7 +2229,7 @@ static int ocfs2_initialize_super(struct super_block *sb,
 		mlog_errno(status);
 		goto bail;
 	}
-	cleancache_init_shared_fs((char *)&uuid_net_key, sb, PAGE_SIZE);
+	cleancache_init_shared_fs((char *)&uuid_net_key, sb);
 bail:
 	mlog_exit(status);
 	return status;
