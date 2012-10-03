8d12e051f030acd0539a62814bde9b39e764ec2a
Dan Magenheimer <dan.magenheimer at>
2012-10-03 22:43:54 +0000

    frontswap/swap: allow frontswap "unuse" and add metadata for tracking it
    We wish for transcendent memory backends to be able to push
    frontswap pages back into the swap cache and need to ensure
    that such a page, once pushed back, doesn't get immediately
    recaptured by frontswap.  We add frontswap_unuse to do the
    pushing via the recently added read_frontswap_async.  We
    also add metadata to track when a page has been pushed and
    code to manage (and count with debugfs) this metadata.
    
    The initialization/destruction code for the metadata (aka
    frontswap_denial_map) is a bit clunky in swapfile.c but
    cleanup can be addressed when all the unuse code is working.
    
    Signed-off-by: Dan Magenheimer <dan.magenheimer <at> oracle.com>

--- include/linux/swap.h
+++ include/linux/swap.h
@@ -200,6 +200,7 @@ struct swap_info_struct {
 	unsigned int old_block_size;	/* seldom referenced */
 #ifdef CONFIG_FRONTSWAP
 	unsigned long *frontswap_map;	/* frontswap in-use, one bit per page */
+	unsigned long *frontswap_denial_map;	/* deny frontswap, 1bit/page */
 	atomic_t frontswap_pages;	/* frontswap pages in-use counter */
 #endif
 };
--- mm/swapfile.c
+++ mm/swapfile.c
@@ -1477,7 +1477,8 @@ static int setup_swap_extents(struct swap_info_struct *sis, sector_t *span)
 
 static void enable_swap_info(struct swap_info_struct *p, int prio,
 				unsigned char *swap_map,
-				unsigned long *frontswap_map)
+				unsigned long *frontswap_map,
+				unsigned long *frontswap_denial_map)
 {
 	int i, prev;
 
@@ -1488,6 +1489,7 @@ static void enable_swap_info(struct swap_info_struct *p, int prio,
 		p->prio = --least_priority;
 	p->swap_map = swap_map;
 	frontswap_map_set(p, frontswap_map);
+	frontswap_denial_map_set(p, frontswap_denial_map);
 	p->flags |= SWP_WRITEOK;
 	nr_swap_pages += p->pages;
 	total_swap_pages += p->pages;
@@ -1589,7 +1591,8 @@ SYSCALL_DEFINE1(swapoff, const char __user *, specialfile)
 		 * sys_swapoff for this swap_info_struct at this point.
 		 */
 		/* re-insert swap space back into swap_list */
-		enable_swap_info(p, p->prio, p->swap_map, frontswap_map_get(p));
+		enable_swap_info(p, p->prio, p->swap_map,
+			frontswap_map_get(p), frontswap_denial_map_get(p));
 		goto out_dput;
 	}
 
@@ -1620,6 +1623,7 @@ SYSCALL_DEFINE1(swapoff, const char __user *, specialfile)
 	mutex_unlock(&swapon_mutex);
 	vfree(swap_map);
 	vfree(frontswap_map_get(p));
+	vfree(frontswap_denial_map_get(p));
 	/* Destroy swap account informatin */
 	swap_cgroup_swapoff(type);
 
@@ -1980,6 +1984,7 @@ SYSCALL_DEFINE2(swapon, const char __user *, specialfile, int, swap_flags)
 	unsigned long maxpages;
 	unsigned char *swap_map = NULL;
 	unsigned long *frontswap_map = NULL;
+	unsigned long *frontswap_denial_map = NULL;
 	struct page *page = NULL;
 	struct inode *inode = NULL;
 
@@ -2064,8 +2069,10 @@ SYSCALL_DEFINE2(swapon, const char __user *, specialfile, int, swap_flags)
 		goto bad_swap;
 	}
 	/* frontswap enabled? set up bit-per-page map for frontswap */
-	if (frontswap_enabled)
-		frontswap_map = vzalloc(maxpages / sizeof(long));
+	if (frontswap_enabled) {
+ 		frontswap_map = vzalloc(maxpages / sizeof(long));
+		frontswap_denial_map = vzalloc(maxpages / sizeof(long));
+	}
 
 	if (p->bdev) {
 		if (blk_queue_nonrot(bdev_get_queue(p->bdev))) {
@@ -2081,7 +2088,8 @@ SYSCALL_DEFINE2(swapon, const char __user *, specialfile, int, swap_flags)
 	if (swap_flags & SWAP_FLAG_PREFER)
 		prio =
 		  (swap_flags & SWAP_FLAG_PRIO_MASK) >> SWAP_FLAG_PRIO_SHIFT;
-	enable_swap_info(p, prio, swap_map, frontswap_map);
+	enable_swap_info(p, prio, swap_map,
+				frontswap_map, frontswap_denial_map);
 
 	printk(KERN_INFO "Adding %uk swap on %s.  "
 			"Priority:%d extents:%d across:%lluk %s%s%s\n",
