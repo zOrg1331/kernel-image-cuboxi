a25e49fce2e6677cea6d5410e56125a59df7c583
Andreas Schwab <schwab@suse.de>
2012-12-17 01:05:42 +0200

    Add compat handler for PTRACE_GETSIGINFO
    Current versions of gdb require a working implementation of
    PTRACE_GETSIGINFO for proper watchpoint support.  Since struct siginfo
    contains pointers it must be converted when passed to a 32-bit debugger.
    
    Signed-off-by: Andreas Schwab <schwab@suse.de>

--- arch/powerpc/kernel/ptrace32.c
+++ arch/powerpc/kernel/ptrace32.c
@@ -28,11 +28,14 @@
 #include <linux/security.h>
 #include <linux/signal.h>
 #include <linux/compat.h>
+#include <linux/elf.h>
 
 #include <asm/uaccess.h>
 #include <asm/page.h>
 #include <asm/pgtable.h>
 #include <asm/switch_to.h>
+
+#include "ppc32.h"
 
 /*
  * does not yet catch signals sent when the child dies.
@@ -68,6 +71,27 @@ static long compat_ptrace_old(struct task_struct *child, long request,
 #define FPRHALF(i) (((i) - PT_FPR0) & 1)
 #define FPRINDEX(i) TS_FPRWIDTH * FPRNUMBER(i) * 2 + FPRHALF(i)
 #define FPRINDEX_3264(i) (TS_FPRWIDTH * ((i) - PT_FPR0))
+
+static int compat_ptrace_getsiginfo(struct task_struct *child, compat_siginfo_t __user *data)
+{
+	siginfo_t lastinfo;
+	int error = -ESRCH;
+
+	read_lock(&tasklist_lock);
+	if (likely(child->sighand != NULL)) {
+		error = -EINVAL;
+		spin_lock_irq(&child->sighand->siglock);
+		if (likely(child->last_siginfo != NULL)) {
+			lastinfo = *child->last_siginfo;
+			error = 0;
+		}
+		spin_unlock_irq(&child->sighand->siglock);
+	}
+	read_unlock(&tasklist_lock);
+	if (!error)
+		return copy_siginfo_to_user32(data, &lastinfo);
+	return error;
+}
 
 long compat_arch_ptrace(struct task_struct *child, compat_long_t request,
 			compat_ulong_t caddr, compat_ulong_t cdata)
@@ -299,6 +323,9 @@ long compat_arch_ptrace(struct task_struct *child, compat_long_t request,
 			child, task_user_regset_view(current), 0,
 			0, PT_REGS_COUNT * sizeof(compat_long_t),
 			compat_ptr(data));
+
+	case PTRACE_GETSIGINFO:
+		return compat_ptrace_getsiginfo(child, compat_ptr(data));
 
 	case PTRACE_GETFPREGS:
 	case PTRACE_SETFPREGS:
