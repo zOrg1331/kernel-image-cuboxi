fdbd2af7aa820cd81257783b5d9c955e40169062
Led <led@altlinux.org>
2013-01-13 23:39:57 +0200

    i915: fix build warning
    drivers/gpu/drm/i915/i915_gem.c: In function 'i915_gem_pwrite_ioctl':
    drivers/gpu/drm/i915/i915_gem.c:797:38: warning: 'ret' may be used uninitialized in this function [-Wmaybe-uninitialized]

--- drivers/gpu/drm/i915/i915_gem.c
+++ drivers/gpu/drm/i915/i915_gem.c
@@ -794,7 +794,7 @@ i915_gem_shmem_pwrite_slow(struct drm_device *dev,
 	ssize_t remain;
 	loff_t offset;
 	char __user *user_data;
-	int shmem_page_offset, page_length, ret;
+	int shmem_page_offset, page_length, ret = 0;
 	int obj_do_bit17_swizzling, page_do_bit17_swizzling;
 
 	user_data = (char __user *) (uintptr_t) args->data_ptr;
