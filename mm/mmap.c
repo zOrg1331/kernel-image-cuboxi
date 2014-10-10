f2822888690234d6d11c5c53586b27817e4d46aa
Anton V. Boyarshinov <boyarsh@altlinux.org>
2014-10-10 12:13:05 +0400

    mm/mmap.c: fix uksm patch for stable change

--- mm/mmap.c
+++ mm/mmap.c
@@ -2770,7 +2770,7 @@ void exit_mmap(struct mm_struct *mm)
 
 	mm->mmap = NULL;
 	mm->mm_rb = RB_ROOT;
-	mm->mmap_cache = NULL;
+	vmacache_invalidate(mm);	
 	up_write(&mm->mmap_sem);
 
 	WARN_ON(atomic_long_read(&mm->nr_ptes) >
