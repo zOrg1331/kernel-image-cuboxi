ef4114c83a03b3610b557c6ef3786b571df03070
Michal Hocko <mhocko@suse.com>
2017-06-14 08:16:54 +0200

    mm: enlarge stack guard gap
    Stack guard page is a useful feature to reduce a risk of stack smashing
    into a different mapping.  We have been using a single page gap which is
    sufficient to prevent having stack adjacent to a different mapping.  But
    this seems to be insufficient in the light of the stack usage in the
    userspace.  E.g.  glibc uses as large as 64kB alloca() in many commonly
    used functions.  Others use constructs liks gid_t buffer[NGROUPS_MAX]
    which is 256kB or stack strings with MAX_ARG_STRLEN.
    
    This will become especially dangerous for suid binaries and the default
    no limit for the stack size limit because those applications can be
    tricked to consume a large portion of the stack and a single glibc call
    could jump over the guard page.  These attacks are not theoretical,
    unfortunatelly.
    
    Make those attacks less probable by increasing the stack guard gap to
    1MB (on systems with 4k pages but make it depend on the page size
    because systems with larger base pages might cap stack allocations in
    the PAGE_SIZE units) which should cover larger alloca() and VLA stack
    allocations.  It is obviously not a full fix because the problem is
    somehow inherent but it should reduce attack space a lot.  One could
    argue that the gap size should be configurable from the userspace but
    that can be done later on top when somebody finds that the new 1MB is
    not suitable or even wrong for some special case applications.
    
    Implementation wise, get rid of check_stack_guard_page and move all the
    guard page specific code to expandable_stack_area which always tries to
    guarantee the gap.  do_anonymous_page then just calls expand_stack.
    Also get rid of stack_guard_page_{start,end} and replace them with
    stack_guard_area to handle stack population and /proc/<pid>/[s]maps.
    
    This should clean up the code which is quite scattered currently and
    therefore justify the change.
    
    Signed-off-by: Michal Hocko <mhocko@suse.com>
    Tested-by: Tony Luck <tony.luck@intel.com> # ia64
    Tested-by: Laura Abbott <labbott@redhat.com>
    Tested-by: Helge Deller <deller@gmx.de> # parisc
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Ben Hutchings <ben@decadent.org.uk>
    [wt: backport to 4.9 : s/vmf->address/fe->address/]
    [wt: backport to 4.4 :
         s/fe->address/address/ ; s/FOLL_POPULATE/FOLL_MLOCK ]
    [wt: backport to 3.10 : adjust context ;
         s/stack_guard_page()/stack_guard_area() ]
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- mm/memory.c
+++ mm/memory.c
@@ -1675,12 +1675,6 @@ no_page_table:
 	return page;
 }
 
-static inline int stack_guard_page(struct vm_area_struct *vma, unsigned long addr)
-{
-	return stack_guard_page_start(vma, addr) ||
-	       stack_guard_page_end(vma, addr+PAGE_SIZE);
-}
-
 /**
  * __get_user_pages() - pin user pages in memory
  * @tsk:	task_struct of target task
@@ -1850,7 +1844,7 @@ long __get_user_pages(struct task_struct *tsk, struct mm_struct *mm,
 
 				/* For mlock, just skip the stack guard page. */
 				if (foll_flags & FOLL_MLOCK) {
-					if (stack_guard_page(vma, start))
+					if (stack_guard_area(vma, start))
 						goto next_page;
 				}
 				if (foll_flags & FOLL_WRITE)
@@ -3230,39 +3224,7 @@ out_release:
 	return ret;
 }
 
-/*
- * This is like a special single-page "expand_{down|up}wards()",
- * except we must first make sure that 'address{-|+}PAGE_SIZE'
- * doesn't hit another vma.
- */
-static inline int check_stack_guard_page(struct vm_area_struct *vma, unsigned long address)
-{
-	address &= PAGE_MASK;
-	if ((vma->vm_flags & VM_GROWSDOWN) && address == vma->vm_start) {
-		struct vm_area_struct *prev = vma->vm_prev;
 
-		/*
-		 * Is there a mapping abutting this one below?
-		 *
-		 * That's only ok if it's the same stack mapping
-		 * that has gotten split..
-		 */
-		if (prev && prev->vm_end == address)
-			return prev->vm_flags & VM_GROWSDOWN ? 0 : -ENOMEM;
-
-		return expand_downwards(vma, address - PAGE_SIZE);
-	}
-	if ((vma->vm_flags & VM_GROWSUP) && address + PAGE_SIZE == vma->vm_end) {
-		struct vm_area_struct *next = vma->vm_next;
-
-		/* As VM_GROWSDOWN but s/below/above/ */
-		if (next && next->vm_start == address + PAGE_SIZE)
-			return next->vm_flags & VM_GROWSUP ? 0 : -ENOMEM;
-
-		return expand_upwards(vma, address + PAGE_SIZE);
-	}
-	return 0;
-}
 
 /*
  * We enter with non-exclusive mmap_sem (to exclude vma changes,
@@ -3284,8 +3246,10 @@ static int do_anonymous_page(struct mm_struct *mm, struct vm_area_struct *vma,
 		return VM_FAULT_SIGBUS;
 
 	/* Check if we need to add a guard page to the stack */
-	if (check_stack_guard_page(vma, address) < 0)
-		return VM_FAULT_SIGSEGV;
+	if (stack_guard_area(vma, address)) {
+		if (expand_stack(vma, address) < 0)
+			return VM_FAULT_SIGSEGV;
+	}
 
 	/* Use the zero-page for reads */
 	if (!(flags & FAULT_FLAG_WRITE)) {
