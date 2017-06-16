031c28ad6d8e99a4c12588c2b7012a548ede9a32
Michal Hocko <mhocko@suse.com>
2017-06-16 12:58:33 +0200

    mm: allow to configure stack gap size
    Add a kernel command line option (stack_guard_gap) to specify the stack
    gap size (in page unites) and export the value in /proc/<pid>/smaps for
    stack vmas.  This might be used for special applications like CRIU/RR.
    
    Changes since v1
     - do not rely on is_stack when reporting the gap. show_map_vma has all
       the information we need
    
    Suggested-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Michal Hocko <mhocko@suse.com>
    Tested-by: Tony Luck <tony.luck@intel.com> # ia64
    Tested-by: Laura Abbott <labbott@redhat.com>
    Tested-by: Helge Deller <deller@gmx.de> # parisc
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    [wt: backport to 4.9 : kernel-parameters.txt is in Documentation/]
    [wt: backport to 3.10 : adjust context]
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -251,7 +251,7 @@ static int do_maps_open(struct inode *inode, struct file *file,
 }
 
 static void
-show_map_vma(struct seq_file *m, struct vm_area_struct *vma, int is_pid)
+show_map_vma(struct seq_file *m, struct vm_area_struct *vma, int is_pid, bool *has_gap)
 {
 	struct mm_struct *mm = vma->vm_mm;
 	struct file *file = vma->vm_file;
@@ -278,11 +278,17 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma, int is_pid)
 	start = vma->vm_start;
 	end = vma->vm_end;
 	if (vma->vm_flags & VM_GROWSDOWN) {
-		if (stack_guard_area(vma, start))
+		if (stack_guard_area(vma, start)) {
 			start = min(end, start + stack_guard_gap);
+			if (has_gap)
+				*has_gap = true;
+		}
 	} else if (vma->vm_flags & VM_GROWSUP) {
-		if (stack_guard_area(vma, end))
+		if (stack_guard_area(vma, end)) {
 			end = max(start, end - stack_guard_gap);
+			if (has_gap)
+				*has_gap = true;
+		}
 	}
 
 	seq_setwidth(m, 25 + sizeof(void *) * 6 - 1);
@@ -353,7 +359,7 @@ static int show_map(struct seq_file *m, void *v, int is_pid)
 	struct proc_maps_private *priv = m->private;
 	struct task_struct *task = priv->task;
 
-	show_map_vma(m, vma, is_pid);
+	show_map_vma(m, vma, is_pid, NULL);
 
 	if (m->count < m->size)  /* vma is copied successfully */
 		m->version = (vma != get_gate_vma(task->mm))
@@ -592,6 +598,7 @@ static int show_smap(struct seq_file *m, void *v, int is_pid)
 		.mm = vma->vm_mm,
 		.private = &mss,
 	};
+	bool has_gap = false;
 
 	memset(&mss, 0, sizeof mss);
 	mss.vma = vma;
@@ -599,7 +606,7 @@ static int show_smap(struct seq_file *m, void *v, int is_pid)
 	if (vma->vm_mm && !is_vm_hugetlb_page(vma))
 		walk_page_range(vma->vm_start, vma->vm_end, &smaps_walk);
 
-	show_map_vma(m, vma, is_pid);
+	show_map_vma(m, vma, is_pid, &has_gap);
 
 	seq_printf(m,
 		   "Size:           %8lu kB\n"
@@ -635,6 +642,9 @@ static int show_smap(struct seq_file *m, void *v, int is_pid)
 	if (vma->vm_flags & VM_NONLINEAR)
 		seq_printf(m, "Nonlinear:      %8lu kB\n",
 				mss.nonlinear >> 10);
+
+	if (has_gap)
+		seq_printf(m, "Stack_Gap:      %8lu kB\n", stack_guard_gap >>10);
 
 	show_smap_vma_flags(m, vma);
 
